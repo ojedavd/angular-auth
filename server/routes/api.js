f42dd919774ef77b700f7a82be1e0f3a4e5be398
Daniel Ojeda <ojedavd@hotmail.com>
2018-10-31 19:23:25 -0300

    Angular Authentication Tutorial - 11 - Events API

--- server/routes/api.js
+++ server/routes/api.js
@@ -47,4 +47,52 @@ router.post('/login', (req, res) => {
     })
 })
 
+router.get('/events', (req, res) => {
+    let events = [
+        {
+            "_id" : "1",
+            "name" : "Auto expo",
+            "description" : "lorem ipsum",
+            "date" : "2012-04-23T18:25:43.511Z",
+        },
+        {
+            "_id" : "2",
+            "name" : "Auto expo",
+            "description" : "lorem ipsum",
+            "date" : "2012-04-23T18:25:43.511Z",
+        },
+        {
+            "_id" : "3",
+            "name" : "Auto expo",
+            "description" : "lorem ipsum",
+            "date" : "2012-04-23T18:25:43.511Z",
+        },
+    ]
+    res.json(events)
+})
+
+router.get('/special', (req, res) => {
+    let events = [
+        {
+            "_id" : "1",
+            "name" : "Auto expo",
+            "description" : "lorem ipsum",
+            "date" : "2012-04-23T18:25:43.511Z",
+        },
+        {
+            "_id" : "2",
+            "name" : "Auto expo",
+            "description" : "lorem ipsum",
+            "date" : "2012-04-23T18:25:43.511Z",
+        },
+        {
+            "_id" : "3",
+            "name" : "Auto expo",
+            "description" : "lorem ipsum",
+            "date" : "2012-04-23T18:25:43.511Z",
+        },
+    ]
+    res.json(events)
+})
+
 module.exports = router
